bb3b723670619686a275a61387e8cfe9c46eeb93
localuser <localuser@VEA.LV>
2023-04-24 11:52:49 +0300

    New: added city class

---
+++ src/model/City.java
@@ -0,0 +1,42 @@
+package model;
+
+public class City {
+	private String title;
+	private String country;
+	private int population;
+	private float squareKM;
+	private int howManyTrafficLights;
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public int getPopulation() {
+		return population;
+	}
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+	public float getSquareKM() {
+		return squareKM;
+	}
+	public void setSquareKM(float squareKM) {
+		this.squareKM = squareKM;
+	}
+	public int getHowManyTrafficLights() {
+		return howManyTrafficLights;
+	}
+	public void setHowManyTrafficLights(int howManyTrafficLights) {
+		this.howManyTrafficLights = howManyTrafficLights;
+	}
+	
+	
+}
